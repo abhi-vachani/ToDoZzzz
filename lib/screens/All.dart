6305ef53db718da35b84e6e68612c2eb09c4a684
Vibhu Singh <vibhu.raj.singh@gmail.com>
2023-02-18 17:08:14 -0500

    Added code from an old different project to add navigation

---
+++ lib/screens/All.dart
@@ -0,0 +1,5 @@
+export './Dining.dart';
+export './Finance.dart';
+export './Home.dart';
+export './Residence.dart';
+export './Tracking.dart';
---
+++ lib/screens/Tracking.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class TrackingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(25.0),
+      child: Text(
+        'Tracking',
+        style: TextStyle(fontSize: 36.0),
+      ),
+    );
+  }
+}
